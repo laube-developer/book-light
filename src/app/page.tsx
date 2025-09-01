5292c3663f19a81119f205de49d5e0acd82c856c
laube-developer <laube.developer@gmail.com>
2025-08-31 22:06:49 -0300

    Icone na tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,13 @@ export default function Home() {
       className="flex flex-col items-center w-full px-4 h-[100dvh] text-slate-900 justify-between"
       style={{background: getReadingLightColor(range)}}>
       <header>
-        <h1 className="text-3xl font-bold p-5 text-center">Light Book</h1>
+        <section className="flex flex-row items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+          </svg>
+
+          <h1 className="text-3xl font-bold p-5 text-center">Light Book</h1>
+        </section>
         <p>Selecione a cor e ilumine a sua leitura</p>
       </header>
 
